61fb2b947bed36656c36ada53843156014a3e137
mfuaddd <mi6vyr5cb@code.edu.az>
2023-11-17 19:26:41 +0400

    aaa

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,31 @@ const headerDepartmentsBtn = document.querySelector(".header_departments_btn")
 const headerDepartmentsMenu = document.querySelector(".header_departments_menu")
 
 headerDepartmentsBtn.addEventListener("click",function(){
-    console.log("aaa");
     headerDepartmentsMenu.classList.toggle("header_departments_menu_anima")
-    // headerDepartmentsMenu.classList.toggle("h-col-12")
 })
+
+const test =document.querySelector(".test")
+const slideList =document.querySelector(".slide_list")
+const slideboxs =document.querySelectorAll(".slide_list li")
+
+function createCounter(num){
+    return (a)=> {
+        if(a) return ++num
+        else return (num=0)
+    }
+}
+
+test.addEventListener("click",function () {
+    if (count === 6){ 
+        count = 0
+    }
+    let width = count * 25
+    slideList.style.transition = "500ms"
+    slideList.style.transform = `translateX(-${width}%)`
+    console.log(count);
+})
+function salam() {
+    slideList.style.transition = "0ms"
+    console.log(slideList.style.transition);
+    slideList.style.transform = `translateX(0%)`
+}
